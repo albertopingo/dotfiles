8051a6a53b5914c0d556c3ff402fb7415b065fba
albertopingo <contact@albertopingo.com>
2025-12-03 18:52:54 +0000

    config: Fedora migration
    Might need extra work in main.yml's, missing packages and simplify cross-distro automation

---
+++ roles/alacritty/files/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+
+local os_name = wezterm.target_triple:find("windows") and "Windows" or "Linux"
+
+local config = {
+	color_scheme = "rose-pine",
+	window_background_opacity = 0.92,
+	hide_tab_bar_if_only_one_tab = true,
+
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 14,
+	tab_max_width = 4,
+
+	max_fps = 144,
+}
+
+if os_name == "Linux" then
+	-- config.enable_wayland = false
+end
+
+if os_name == "Windows" then
+	config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+end
+
+return config
